cb88fa66f7ad45f6ca3262bdbd90c63caf9c3f01
Rafael Almendra <rafaelalmendra28@gmail.com>
2022-12-14 10:25:46 -0300

    refactor: change script hotjar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import { ThemeProvider } from "styled-components";
+import { useState } from 'react';
+import Script from 'next/script';
+import { ThemeProvider } from 'styled-components';
 
-import { lightTheme, darkTheme } from "../styles/theme";
-import GlobalStyle from "../styles/global";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import "swiper/swiper.scss";
-import "swiper/swiper-bundle.css";
+import { lightTheme, darkTheme } from '../styles/theme';
+import GlobalStyle from '../styles/global';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import 'swiper/swiper.scss';
+import 'swiper/swiper-bundle.css';
 
 export default function MyApp({ Component, pageProps }) {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
@@ -17,6 +18,23 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <Script
+        id="hotjar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3288324,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
+
       <Navbar toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
       <Component {...pageProps} />
       <Footer />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -30,23 +29,8 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html lang="pt-br">
+      <Html lang="en">
         <Head>
-          <Script
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3288324,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `,
-            }}
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <meta charSet="utf-8" />
